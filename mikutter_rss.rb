4b8e55fa83be2352837619b71dcae7a75534adae
yukkuri_sinai <yukkuri_sinai@yukkuriPC.(none)>
2013-07-02 18:00:32 +0900

    change gsub

--- mikutter_rss.rb
+++ mikutter_rss.rb
@@ -3,7 +3,7 @@
 require 'rss'
 
 Plugin.create(:mikutter_rss) do
-  
+
   def reload
     #更新を行う
     #ただし これだと複数のRSSが別々で並ぶので，完全な時系列にならなく見にくい可能性がある
@@ -11,7 +11,7 @@ Plugin.create(:mikutter_rss) do
     (UserConfig[:rss_url]|| []).select{|m|!m.empty?}.each do |url|
       rss = RSS::Parser.parse(url)
       rss.items.each{|item|
-        timeline(:mikutter_rss) << Message.new(:message => "#{item.title.gsub(/<\/?[^>]*>/, "").gsub("\n\n","\n")}\n#{item.description.gsub(/<\/?[^>]*>/, "").gsub("\n\n","\n")}\n#{item.link}", :system => true)
+        timeline(:mikutter_rss) << Message.new(:message => "#{item.title.gsub(/<\/?[^>]*>/, "")}\n#{item.description.gsub(/<\/?[^>]*>/, "")}\n#{item.link}", :system => true)
       }
     end
   end
