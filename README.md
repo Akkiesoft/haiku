1928ee5fbb25431e138469c13312675329563f53
Akira Ouchi <akkiesoft@marokun.net>
2014-08-15 13:22:54 +0900

    URL記法をリンクに対応/スレッドっぽい動きの復活(Thanks mikutter_niconico plugin)

--- README.md
+++ README.md
@@ -13,7 +13,8 @@ mikutterから簡易的に[はてなハイク][hatenahaiku]を扱うためのプ
 3. 設定画面の「はてなハイク」で、ハイクJSON URLに購読したいタイムラインのJSON URLを入力します。書式は[はてなハイク REST API][haiku_rest_api]のタイムラインAPIの各ページを参考にしてください
 4. 設定画面の「はてなハイク」にある他のオプションは、おこのみで設定します
 5. 投稿コマンドは適宜ショートカットキーを割り当てます
-6. ハイクタブで閲覧できたり、ハイクに投稿ができたら、これであなたも爽やかておくれライフ
+6. 抽出タブでハイク用のタブ作ります。NGIDも自由自在！
+7. タブでハイクを閲覧したり、ハイクに投稿ができたら、これであなたも爽やかておくれライフ
 
 ####コマンド割り当て例
 通常の投稿がCtrl+Enterに割り当てられている場合、次のような設定ではいかがでしょうか。
@@ -29,6 +30,7 @@ mikutterから簡易的に[はてなハイク][hatenahaiku]を扱うためのプ
 + <http://toshia.github.io/writing-mikutter-plugin/>
 + <http://mikutter.blogspot.jp/2011/11/ui.html>
 + <https://github.com/penguin2716/mikutter_update_with_media>
++ <https://github.com/shibafu528/mikutter_niconico>
 + そのたいろいろ
 
 [hatenahaiku]: http://h.hatena.ne.jp/
--- mikutter_haiku.rb
+++ mikutter_haiku.rb
@@ -16,6 +16,7 @@ Plugin.create(:mikutter_haiku) do
 
   defactivity "mikutter_haiku", "はてなハイク"
 
+  # TODO: warning: class variable access from toplevelなので対処する
   @@haiku_lastupdate = 0;
 
   ########################################
@@ -50,152 +51,137 @@ Plugin.create(:mikutter_haiku) do
   ########################################
   ## Reader :: リロード処理
   ##
-  def reload
+  def reload_haiku
     (UserConfig[:haiku_url]|| []).select{|m|!m.empty?}.each do |url|
-      # それぞれ別スレッドとして実行する
-      #Thread.fork {
-        begin
-         # Gtk::Dialog.alert("YES")
-          now = Time.now.to_i
-          if @@haiku_lastupdate == 0 then
-            timeline = "&reftime=-#{now},0"
-          else
-            timeline = "&reftime=+#{@@haiku_lastupdate},0"
-          end
-          uri = URI.parse("#{url}?body_formats=haiku#{timeline}")
-          json = Net::HTTP.get(uri)
-          items = JSON.parse(json)
-        rescue => ee
-        # パースに失敗した場合は例外引っ掛けてスルー
-          activity :mikutter_haiku, "JSONのパースに失敗しました\n#{url}?body_formats=haiku\n#{ee}"
+      begin
+        now = Time.now.to_i
+        if @@haiku_lastupdate == 0 then
+          timeline = "&reftime=-#{now},0"
         else
-          i = 0
-          items.each do |item|
-            msgs = Messages.new
-            keyword		= item['target']['title']
-            body		= item['haiku_text']
-            link		= item['link']
-            source		= item['source']
-            time		= Time.parse(item['created_at']).localtime
-	
-            # URL記法対応
-            sintaxes = body.split('[')
-            if sintaxes[0] != body then
-              sintaxes.each do |sintax|
-                pos = sintax.index(']')
-                if pos.nil? then
-                  next		# これは記法ではない
-                end
-                sintax = sintax.slice(0, pos);
-                sintaxOut = sintax.gsub(/(https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)\:title=(.+)/, "\\2( \\1 )");
-                body = body.gsub("[#{sintax}]", "#{sintaxOut}");
-              end
-            end
-	
-            # はてなフォトライフ
-            sintaxes = body.scan(/f:id:([-_a-zA-Z0-9]+):([0-9]{8})([0-9]{6})(j|g|p|f)?(:image|:movie)?/i) {|match|
-              foto_id		= match[0];
-              foto_initial	= foto_id.slice(0, 1);
-              foto_date		= match[1];
-              foto_time		= match[2];
-              foto_type		= (defined?(match[3])) ? match[3] : '';
-              foto_mode		= (defined?(match[4])) ? match[4] : '';
-              foto_ext		= 'jpg';
-              foto_ext		= 'gif' if foto_type == 'g'
-              foto_ext		= 'png' if foto_type == 'p'
-              foto_org		= "f:id:#{foto_id}:#{foto_date}#{foto_time}"
-              foto_org		+= foto_type if foto_type
-              foto_org		+= foto_mode if foto_mode
-              foto_img		= "http://cdn-ak.f.st-hatena.com/images/fotolife/#{foto_initial}/#{foto_id}/#{foto_date}/#{foto_date}#{foto_time}.#{foto_ext}"
-              foto_link		= "http://f.hatena.ne.jp/#{foto_id}/#{foto_date}#{foto_time}"
-              if foto_type == "f" && foto_mode == ":movie" then
-                body = body.sub("#{foto_org}", "#{foto_link}")
-              else
-                body = body.sub("#{foto_org}", "#{foto_img}")
-              end
-            }
-            message_head = "#{item['user']['screen_name']} (Permalink)\n\n"
-            message_text = "#{message_head}<#{keyword}>\n#{body}"
-            entities = {
-                urls: [
-                  {
-                    url: item['user']['screen_name'],
-                    expanded_url: item['user']['url'], 
-                    display_url: item['user']['screen_name'],
-                    indices: [0, message_head.length]
-                  },
-                  {
-                    url: "(Permalink)",
-                    expanded_url: link, 
-                    display_url: "(Permalink)",
-                    indices: [0, message_head.length]
-                  },
-                  {
-                    url: "<#{keyword}>",
-                    expanded_url: "http://h.hatena.ne.jp/target?word=#{keyword}", 
-                    display_url: "<#{keyword}>",
-                    indices: [message_head.length, message_head.length + "<#{keyword}>".length + 2]
-                  },
-                ],
-                symbols: [],
-                hashtags: [],
-                user_mentions: []
-            }
-            user   = User.new({
-              # :idはハイクに数値IDが存在しないのでハッシュでごまかす
-              :id					=> "#{item['user']['id']}".hash,
-              :idname				=> item['user']['screen_name'],
-              :name					=> item['user']['name'],
-              :nickname				=> item['user']['screen_name'],
-              :profile_image_url	=> item['user']['profile_image_url'],
-              :detail				=> "",
-              :url					=> item['user']['url']
-            })
-            msgs << Message.new({
-              :id => time.to_i,
-              :message => message_text,
-              :user => user,
-              :source => source,
-              :created => time,
-              :entities => entities
-            })
-            i += 1
-            Plugin.call(:extract_receive_message, :mikutter_haiku, msgs)
-          end
-	
-          # 最後に実行した時間を記録
-          @@haiku_lastupdate = now
+          timeline = "&reftime=+#{@@haiku_lastupdate},0"
         end
-	  #}
+        uri = URI.parse("#{url}?body_formats=haiku#{timeline}")
+        json = Net::HTTP.get(uri)
+        items = JSON.parse(json)
+      rescue => ee
+        # パースに失敗した場合は例外引っ掛けてスルー
+        activity :mikutter_haiku, "JSONのパースに失敗しました\n#{url}?body_formats=haiku\n#{ee}"
+      else
+        parse(items)
+        # 最後に実行した時間を記録
+        @@haiku_lastupdate = now
+        if (UserConfig[:haiku_auto]) then
+          Reserver.new(60) {
+            reload_haiku
+          }
+        end
+      end
     end
   end
 
   ########################################
-  ## Reader :: タブの作成
+  ## Reader :: パース処理
   ##
-#  btn = Gtk::Button.new('更新')
-#  tab(:mikutter_haiku, 'はてなハイク') do
-#    set_icon File.expand_path(File.join(File.dirname(__FILE__), 'logo.png'))
-#    shrink
-#    nativewidget Gtk::HBox.new(false, 0).closeup(btn)
-#    expand
-#    timeline :mikutter_haiku
-#  end
+  def parse(items)
+    items.each do |item|
+      msgs = Messages.new
+      keyword	= item['target']['title']
+      body		= item['haiku_text']
+      link		= item['link']
+      source	= item['source']
+      time		= Time.parse(item['created_at']).localtime
 
-  ########################################
-  ## Reader :: 更新ボタンがクリックされた時
-  ##
-#  btn.signal_connect('clicked'){ |elm|
-#    reload
-#  }
+      # はてなフォトライフ
+      sintaxes = body.scan(/f:id:([-_a-zA-Z0-9]+):([0-9]{8})([0-9]{6})(j|g|p|f)?(:image|:movie)?/i) {|match|
+        foto_id			= match[0];
+        foto_initial	= foto_id.slice(0, 1);
+        foto_date		= match[1];
+        foto_time		= match[2];
+        foto_type		= (defined?(match[3])) ? match[3] : '';
+        foto_mode		= (defined?(match[4])) ? match[4] : '';
+        foto_ext		= 'jpg';
+        foto_ext		= 'gif' if foto_type == 'g'
+        foto_ext		= 'png' if foto_type == 'p'
+        foto_org		= "f:id:#{foto_id}:#{foto_date}#{foto_time}"
+        foto_org		+= foto_type if foto_type
+        foto_org		+= foto_mode if foto_mode
+        foto_img		= "http://cdn-ak.f.st-hatena.com/images/fotolife/#{foto_initial}/#{foto_id}/#{foto_date}/#{foto_date}#{foto_time}.#{foto_ext}"
+        foto_link		= "http://f.hatena.ne.jp/#{foto_id}/#{foto_date}#{foto_time}"
+        if foto_type == "f" && foto_mode == ":movie" then
+          body = body.sub("#{foto_org}", "#{foto_link}")
+        else
+          body = body.sub("#{foto_org}", "#{foto_img}")
+        end
+      }
 
-  ########################################
-  ## Reader :: 1分に1度 自動で更新
-  ##
-  on_period do
-    # 起動時にもon_periodが来るので、起動時に云々はここでやる
-    if(UserConfig[:haiku_auto] || (@@haiku_lastupdate == 0 && UserConfig[:haiku_exec]))
-      reload
+      # Entitiesの作成
+      message_head = "#{item['user']['screen_name']} (Permalink)\n\n"
+      message_text = "#{message_head}<#{keyword}>\n#{body}"
+      entities = {
+        urls: [
+          {
+            url: item['user']['screen_name'],
+            expanded_url: item['user']['url'],
+            display_url: item['user']['screen_name'],
+            indices: [0, message_head.length]
+          },
+          {
+            url: "(Permalink)",
+            expanded_url: link,
+            display_url: "(Permalink)",
+            indices: [0, message_head.length]
+          },
+          {
+            url: "<#{keyword}>",
+            expanded_url: "http://h.hatena.ne.jp/target?word=#{keyword}",
+            display_url: "<#{keyword}>",
+            indices: [message_head.length, message_head.length + "<#{keyword}>".length + 2]
+          },
+        ],
+        symbols: [],
+        hashtags: [],
+        user_mentions: []
+      }
+
+      # URL記法対応
+      sintaxes = body.split('[')
+      if sintaxes[0] != body then
+        sintaxes.each do |sintax|
+          pos = sintax.index(']')
+          if pos.nil? then
+            next		# これは記法ではない
+          end
+          sintax = sintax.slice(0, pos);
+          sintaxUrl = sintax.gsub(/(https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)\:title=(.+)/, "\\1");
+          sintaxTitle = sintax.gsub(/(https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)\:title=(.+)/, "\\2");
+          entities[:urls].concat([{
+            url: "[#{sintax}]",
+            expanded_url: sintaxUrl,
+            display_url: sintaxTitle,
+            indices: [0, message_text.length]	# てぬき
+          }])
+        end
+      end
+
+      user = User.new({
+        # :idはハイクに数値IDが存在しないのでハッシュでごまかす
+        :id					=> "#{item['user']['id']}".hash,
+        :idname				=> item['user']['screen_name'],
+        :name				=> item['user']['name'],
+        :nickname			=> item['user']['screen_name'],
+        :profile_image_url	=> item['user']['profile_image_url'],
+        :url				=> item['user']['url'],
+        :detail				=> ""
+      })
+      msgs << Message.new({
+        :id			=> time.to_i,
+        :message	=> message_text,
+        :user		=> user,
+        :source		=> source,
+        :created	=> time,
+        :entities	=> entities
+      })
+      Plugin.call(:extract_receive_message, :mikutter_haiku, msgs)
     end
   end
 
@@ -213,7 +199,7 @@ Plugin.create(:mikutter_haiku) do
 		defactivity "Haiku_post", "Haiku_Post"
 		activity :Haiku_Post, "ハイクに投稿しました"
 		Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = ''
-        reload
+        reload_haiku
 	end
   end
 
@@ -232,14 +218,8 @@ Plugin.create(:mikutter_haiku) do
 		defactivity "Haiku_post", "Haiku_Post"
 		activity :Haiku_Post, "ハイクとTwitterに投稿しました"
 		Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = ''
-        reload
+        reload_haiku
 	end
-  end
-
-
-  filter_extract_datasources do |datasources|
-      datasources[:mikutter_haiku] = "はてなハイク"
-      [datasources]
   end
 
   ########################################
@@ -251,10 +231,24 @@ Plugin.create(:mikutter_haiku) do
       input("APIパスワード",:hatena_api_pass)
     end
     settings "タイムライン" do
-      boolean('起動時に更新する', :haiku_exec)
       boolean('1分毎に自動更新を行う', :haiku_auto)
       multi "ハイクJSON URL", :haiku_url
     end
   end
+
+  ########################################
+  ## Reader :: データソース出力の設定
+  ##
+  filter_extract_datasources do |datasources|
+      datasources[:mikutter_haiku] = "はてなハイク"
+      [datasources]
+  end
+
+  ########################################
+  ## Reader :: スタート
+  ##
+  SerialThread.new {
+    reload_haiku
+  }
 
 end
