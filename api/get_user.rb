13ee8f2b1ae934572f07aa8732e38ef75d14d80d
Akira Ouchi <akkiesoft@marokun.net>
2018-01-11 01:28:28 +0900

    get_userをモジュール化

--- api/get_user.rb
+++ api/get_user.rb
@@ -2,12 +2,15 @@
 
 module Plugin::Haiku
 
-  def get_user(hatena_id, hatena_api_pass)
+  def get_user(hatena_id, hatena_api_pass, count = 20)
+    count = "?count=#{count}"
     res = Net::HTTP.start("h.hatena.ne.jp") { |http|
-      req = Net::HTTP::Get.new("/api/statuses/user_timeline.json")
+      req = Net::HTTP::Get.new("/api/statuses/user_timeline.json#{count}")
       req.basic_auth(hatena_id, hatena_api_pass)
       http.request(req)
     }
+    JSON.parse(res.body)[0]["user"]
   end
 
+  module_function :get_user
 end
