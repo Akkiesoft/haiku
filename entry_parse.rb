2dbc4a2e732c138a36a73310767708af641de9a7
Akira Ouchi <akkiesoft@marokun.net>
2018-01-29 23:49:38 +0900

    フォトライフ記法をURLに書き換えるのをやめてEntity化
    フォトライフ記法は文字列をそのままにして
    EntityでフォトライフのURLにリンクするようにした
    (:slugはhatenafotolifeとか勝手に増やしてみた。)
    その他、コメントとインデントの調整、本文の変数の扱いを簡素化

--- entry_parse.rb
+++ entry_parse.rb
@@ -7,19 +7,10 @@ module Plugin::Haiku
       id		= item['id']
       keyword	= item['target']['title']
       keyword_url = "http://h.hatena.ne.jp/target?word=" + URI.encode_www_form_component(keyword)
-      body		= item['haiku_text']
+      body		= "<#{keyword}>\n#{item['haiku_text']}"
       link		= item['link']
       source	= item['source']
       time		= Time.parse(item['created_at']).localtime
-
-      # はてなフォトライフ
-      body.scan(/f:id:([-_a-zA-Z0-9]+):([0-9]{8})([0-9]{6})(j|g|p|f)?(:image|:movie)?/i) {
-        match = Regexp.last_match
-        body = body.sub(
-          "#{match.to_s}",
-          "http://f.hatena.ne.jp/#{match[1]}/#{match[2]}#{match[3]}"
-        )
-      }
 
       user = Plugin::Haiku::User.new({
         # :idはハイクに数値IDが存在しないのでハッシュでごまかす
@@ -32,33 +23,57 @@ module Plugin::Haiku
         detail: ""
       })
 
-      message_text = "<#{keyword}>\n#{body}"
-
       message = Plugin::Haiku::Entry.new({
         id: id,
-        message: message_text,
+        message: body,
         user: user,
         link: link,
         source: source,
         created: time
       })
 
-      # Entitiesの作成
-      message.entity.add(slug: :urls,
-                         open: keyword_url,
-                         url: "",
-                         face: "<#{keyword}>",
-                         range: 0...("<#{keyword}>".length))
+      # キーワードのEntity作成
+      message.entity.add(
+        slug: :urls,
+        url: "",
+        open: keyword_url,
+        face: "<#{keyword}>",
+        range: 0...("<#{keyword}>".length))
 
-      # URL記法対応
-      message_text.gsub(/\[(https?:\/\/[-_.!~*\'\(\)a-zA-Z0-9;\/?:\@&=+\$,%#]+)\:title=(.+)\]/) do
+      # はてなフォトライフ記法対応
+      body.gsub(/f:id:([-_a-zA-Z0-9]+):([0-9]{8})([0-9]{6})(j|g|p|f)?(:image|:movie)?/i) do
         match = Regexp.last_match
         pos = match.begin(0)
-        message.entity.add(slug: :urls,
-                           open: match[1],
-                           url: match[1],
-                           face: match[2],
-                           range: pos...(pos + match.to_s.size))
+        url = "http://f.hatena.ne.jp/#{match[1]}/#{match[2]}#{match[3]}"
+        ext = ""
+        case match[4]
+        when "j" then
+          ext = "jpg"
+        when "g" then
+          ext = "gif"
+        when "p" then
+          ext = "png"
+        end
+        expanded_url = ext ? "https://cdn-ak.f.st-hatena.com/images/fotolife/#{match[1][0]}/#{match[1]}/#{match[2]}/#{match[2]}#{match[3]}.#{ext}" : url
+        message.entity.add(
+          slug: :hatenafotolife,
+          open: url,
+          url: url,
+          expanded_url: expanded_url,
+          face: match[0],
+          range: pos...(pos + match.to_s.size))
+      end
+
+      # URL記法対応
+      body.gsub(/\[(https?:\/\/[-_.!~*\'\(\)a-zA-Z0-9;\/?:\@&=+\$,%#]+)\:title=(.+)\]/) do
+        match = Regexp.last_match
+        pos = match.begin(0)
+        message.entity.add(
+          slug: :urls,
+          open: match[1],
+          url: match[1],
+          face: match[2],
+          range: pos...(pos + match.to_s.size))
       end
 
     #  msgs << message
